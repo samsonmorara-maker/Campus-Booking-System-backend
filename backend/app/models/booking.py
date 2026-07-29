e50fcacf36c546df54c8c2a34a5694da9856698e
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-07-29 11:43:02 +0300

    models of books and schedule done

---
+++ backend/app/models/booking.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from app.extensions import db
+
+
+class Booking(db.Model):
+    __tablename__ = "bookings"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column( db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    facility_id = db.Column(db.Integer, db.ForeignKey("facilities.id"), nullable=False)
+
+    booking_date = db.Column(db.Date, nullable=False)
+
+    start_time = db.Column(db.Time, nullable=False)
+
+    end_time = db.Column(db.Time, nullable=False)
+
+    status = db.Column(db.String(20), nullable=False, default="Pending")
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship("User", back_populates="bookings")
+    
+    facility = db.relationship("Facility", back_populates="bookings")
+
+    def __repr__(self):
+        return f"<Booking {self.id}>"
---
+++ backend/app/models/class_schedule.py
@@ -0,0 +1,51 @@
+from app import db
+
+
+class ClassSchedule(db.Model):
+    __tablename__ = "class_schedules"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    facility_id = db.Column(
+        db.Integer,
+        db.ForeignKey("facilities.id"),
+        nullable=False
+    )
+
+    course_name = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    lecturer = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    day = db.Column(
+        db.String(20),
+        nullable=False
+    )
+
+    start_time = db.Column(
+        db.Time,
+        nullable=False
+    )
+
+    end_time = db.Column(
+        db.Time,
+        nullable=False
+    )
+
+    semester = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+    facility = db.relationship(
+        "Facility",
+        backref="class_schedules"
+    )
+
+    def __repr__(self):
+        return f"<ClassSchedule {self.course_name}>"
